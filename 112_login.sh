23fd442aa10ad70f9de0038c8ece76406139ed94
huangzhw <doggy1853@163.com>
2019-07-11 10:34:36 +0800

    提交113/112的选择登录脚本

--- 112_login.sh
+++ 112_login.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+mentionStr="112 Host message list\n1. 10.112.32.64 fs-marketing-provider fs-marketing-task fs-marketing-statistic-provider fs-enterprise-relation-all-provider\n2. 10.112.32.23 wechat-union相关 wechat-sender-provider\n3. 10.112.32.51 wechat-proxy相关\no. 手工输入ip"
 source `dirname "$0"`/.pwd
-mentionStr="112 Host message list\n1. 10.112.32.64 fs-marketing-provider fs-marketing-task fs-marketing-statistic-provider fs-enterprise-relation-all-provider\n2. 10.112.32.23 wechat-union相关 wechat-sender-provider\n3. 10.112.32.51 wechat-proxy相关"
 pwd=$FIRSTSHARE_PWD
 while true
 do 
 	echo -e $mentionStr
 	echo "Please pick one host to login:"
 	exitRe="^exit$"
+	otherRe="^o$"
 	numberRe='^[0-9]+$'
 	read hostNo
+	# do exit
 	if [[ $hostNo =~ $exitRe ]]
 	then exit
+	fi
+	# do manual input ip
+	if [[ $hostNo =~ $otherRe ]]
+	then
+		echo "Please input ip:"
+		read ip
+		sshpass -p "$pwd" ssh $ip
 	fi
 	#only number passed
 	if ! [[ $hostNo =~ $numberRe ]]
--- 113_login.sh
+++ 113_login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mentionStr="113 Host message list\n1. 10.113.32.64 fs-marketing-provider fs-marketing-task fs-marketing-statistic-provider fs-enterprise-relation-all-provider\n2. 10.113.32.27 wechat-union相关 wechat-sender-provider\n3. 10.113.32.51 wechat-proxy相关"
+mentionStr="113 Host message list\n1. 10.113.32.64 fs-marketing-provider fs-marketing-task fs-marketing-statistic-provider fs-enterprise-relation-all-provider\n2. 10.113.32.27 wechat-union相关 wechat-sender-provider\n3. 10.113.32.51 wechat-proxy相关\no. 手工输入ip"
 source `dirname "$0"`/.pwd
 pwd=$FIRSTSHARE_PWD
 while true
@@ -7,10 +7,19 @@ do
 	echo -e $mentionStr
 	echo "Please pick one host to login:"
 	exitRe="^exit$"
+	otherRe="^o$"
 	numberRe='^[0-9]+$'
 	read hostNo
+	# do exit
 	if [[ $hostNo =~ $exitRe ]]
 	then exit
+	fi
+	# do manual input ip
+	if [[ $hostNo =~ $otherRe ]]
+	then
+		echo "Please input ip:"
+		read ip
+		sshpass -p "$pwd" ssh $ip
 	fi
 	#only number passed
 	if ! [[ $hostNo =~ $numberRe ]]
